74a0fb4ec0bec3a80c5ed2e896422a0650f75c9f
mats3 <faktorx24@gmail.com>
2022-07-08 20:20:26 +0200

    ya moinw

--- src/filestats/filestats.h
+++ src/filestats/filestats.h
@@ -3,17 +3,9 @@
 
 #include"simptypes.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* Return error Codes */
 #define FILE_NOT_EXIST -1 /* for the count_lines function */
 
 int filestats_file_count_lines(char *file_path); /* Returns the amount of lines of a File or FILE_NOT_EXIST when don't exist*/
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,7 +4,12 @@
 
 int main(int argc, char **argv)
 {
-	uint total_lines = 0; /* store the total of lines for all files */
+	/* check for enough arguments */
+	if (argc < 2)
+	{
+		printf("please give me some arguments!!!\n");
+		return 0;
+	}
 
 	/* 
 	 * loop for all arguments without the first one,
@@ -13,20 +18,7 @@ int main(int argc, char **argv)
 	 */
 	void *end;
 	for (end = argv++ + argc; argv != end; argv++)
-	{
-		int lines = filestats_file_count_lines(*argv); /* gets the lines of the file */
-		
-		if (lines == FILE_NOT_EXIST)
-		{
-			printf("--File: %s\nerror: %s", *argv, "File don't exist's");
-		} else
-		{
-			total_lines += lines; /* add the lines to the total lines */
-			printf("--File: %s\nlines: %d\n\n", *argv, lines); /* print the Filename and the lines of the File */
-		}
-	}
-
-	printf("Total Lines: %d\n", total_lines); /* printout the total lines from all files */
+		filestats_file_count_lines(*argv); /* gets the lines of the file */
 
 	return 0;
 }
